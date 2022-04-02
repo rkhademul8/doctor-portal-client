57018cc43714c60d5e6f23c04b9add3a7b5c9aa1
rkhademul8 <rkhademul8.com>
2022-04-02 18:57:52 +0600

    appoinment_design_done

---
+++ src/Pages/Home/AppoinmentBanner/AppoinmentBanner.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import doctor from "../../../images/doctor.png"
+import bg from "../../../images/appointment-bg.png"
+import { Button, Container, Typography } from '@mui/material';
+
+const appoinmentBanner={
+    background: `url(${bg})`,
+    backgroundColor: 'rgba(45,58,74,0.8)',
+    backgroundBlendMode:'darken, luminosity',
+    marginTop:175,
+}
+
+
+const AppoinmentBanner = () => {
+    return (
+       <Container>
+            <Box style={appoinmentBanner} sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <img style={{width:'400px', marginTop:'-110px'}} src={doctor} alt="" />
+          </Grid>
+          <Grid item xs={12} md={6} sx={{display:'flex',justifyContent: 'flex-start' ,
+          alignItems:'center',
+          textAlign:'left'
+        }}>
+            
+            <Box >
+            <Typography variant='h6' sx={{mb:3}} style={{color:'#5CE7ED'}}>
+                Appoinment
+            </Typography>
+            <Typography variant='h4' sx={{mb:3}}  style={{color:'white'}}>
+               Make an Appoinment TOday
+            </Typography>
+            <Typography variant='h6' sx={{mb:3}}  style={{color:'white', fontSize:14, fontWeight:300}}>
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti aliquam ullam hic perferendis aliquid fugit temporibus dolores repudiandae incidunt ut.
+            </Typography>
+            <Button variant='contained'>Learn More</Button>
+            </Box>
+
+          </Grid>
+        </Grid>
+      </Box>
+       </Container>
+    );
+};
+
+export default AppoinmentBanner;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Button, Container, Typography } from '@mui/material';
+import chair from '../../../images/chair.png'
+import bg from '../../../images/bg.png'
+
+
+const BannerBg={
+    background: `url(${bg})`,
+    backgroundColor: 'rgba(45,58,74,0.4)',
+    backgroundBlendMode:'darken, luminosity',
+    marginTop:175,
+}
+
+const verticalCenter={
+    display:'flex',
+    height:400,
+    alignItems:'center',
+}
+
+const Banner = () => {
+    return (
+   <Container style={BannerBg}>
+            <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid style={{...verticalCenter, textAlign:'left'}} item xs={12} md={6}>
+
+             <Box>
+
+             <Typography sx={{mb:3}} variant='h4'>
+                    Your new Smile
+                    Starts Here
+              </Typography>
+              <Typography  variant='h6' sx={{fontSize:14, color:'white', fontWeight:300,mb:3}}>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam facilis inventore temporibus repudiandae dolore cumque! Quod sequi doloremque sunt culpa?
+              </Typography>
+              <Button variant='contained'> Get Appoinment</Button>
+
+             </Box>
+            
+          </Grid>
+          <Grid item xs={12} md={6} style={verticalCenter}>
+            <img style={{width:'400px'}} src={chair} alt="" />
+          </Grid>
+          
+        </Grid>
+      </Box>
+   </Container>
+    );
+};
+
+export default Banner;
