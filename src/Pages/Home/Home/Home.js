566557ac0eef90b3bd46871202101ac1c3dd0e01
rkhademul8 <rkhademul8.com>
2022-04-02 11:43:25 +0600

    services_design_done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
-            This is Home
+            <Navigation></Navigation>
+            <Services></Services>
         </div>
     );
 };
